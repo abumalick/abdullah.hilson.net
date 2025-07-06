6251abc5f59b8b8537f8ca6cbfa4319d257c5988
abumalick <abdullah.hilson@gmail.com>
2025-07-06 23:00:00 +0300

    chore: change socials order

--- src/config.ts
+++ src/config.ts
@@ -21,20 +21,20 @@ export const LOGO_IMAGE = {
 
 export const SOCIALS: SocialObjects = [
   {
-    name: "Github",
-    href: "https://github.com/abumalick",
-    linkTitle: ` ${SITE.title} on Github`,
-    active: true,
-  },
-  {
     name: "LinkedIn",
     href: "https://www.linkedin.com/in/abdullah-hilson-565a1455/",
     linkTitle: `${SITE.title} on LinkedIn`,
     active: true,
   },
   {
+    name: "Github",
+    href: "https://github.com/abumalick",
+    linkTitle: ` ${SITE.title} on Github`,
+    active: true,
+  },
+  {
     name: "Mail",
-    href: "mailto:abdullah.hilson@gmail.com",
+    href: "mailto:abdullah@hilson.net",
     linkTitle: `Send an email to ${SITE.title}`,
     active: false,
   },
